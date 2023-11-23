4db4fefcf5778b9fe79a75c1acbfbe7daec3e53d
akademiyaTOP <dk26.01.1999@yandex.ru>
2023-11-23 14:26:06 +0300

    добавлен вывод переменной a

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int a = 0;
+
+for (int i = 0; i < 10; i++)
+{
+    a++;
+    Console.WriteLine(a);
+}
+Console.WriteLine("hihihihihihihihihih");
+
+
